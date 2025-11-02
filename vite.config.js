6d92e970212c19129b873d4f8112d826aff0b2b6
montassar.kebsi <kebsimontassar@gmail.com>
2025-11-03 00:18:31 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/Montassar/',
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
